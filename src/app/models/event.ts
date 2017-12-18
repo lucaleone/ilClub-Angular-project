140ea420bd46150b826d36d7ed9d85a973993dd2
Luca Leone <lucaleone@outlook.com>
2017-12-18 01:00:44 +0100

    optimized images

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -1,13 +1,12 @@
 export class Event {
-  constructor(public key: string,
-              public  data: string,
-              public  id: number,
-              public  descrizione: string,
-              public  immagine: string,
-              public  ora: string,
-              public  owner: string,
-              public  partecipanti: string,
-              public  sede: string,
-              public  titolo: string) {
-  }
+  public key: string;
+  public data: string;
+  public id: number;
+  public descrizione: string;
+  public immagine: string;
+  public ora: string;
+  public owner: string;
+  public partecipanti: string;
+  public sede: string;
+  public titolo: string;
 }
